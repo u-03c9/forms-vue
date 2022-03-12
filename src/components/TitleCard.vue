e9770523699c11d5764237cce4ea3b611776e60d
Peter Khalil <uni.03c9@gmail.com>
2022-03-12 16:41:47 +0200

    fixing some style issues with title card

--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -8,15 +8,15 @@ defineEmits(["update:title", "update:description"]);
 <template>
   <BaseCard hasTopBar>
     <template v-slot>
-      <div class="w-full h-full pt-6 pb-6 px-5">
+      <div class="w-full h-full pt-8 pb-6 px-6">
         <input
-          class="w-full outline-none border-b-2 pb-2 border-slate-300 text-3xl focus:border-[#673ab7]"
+          class="w-full outline-none border-b-[1px] pb-2 border-gray-300 text-3xl focus:border-[#673ab7]"
           :value="title"
           @input="$emit('update:title', ($event.target as HTMLInputElement).value)"
         />
         <input
           placeholder="Form description"
-          class="w-full outline-none border-b-2 pb-1 border-slate-300 text-md mt-3 focus:border-[#673ab7]"
+          class="w-full outline-none border-b-[1px] pb-1 border-gray-300 text-md mt-3 focus:border-[#673ab7]"
           :value="description"
           @input="$emit('update:description', ($event.target as HTMLInputElement).value)"
         />
