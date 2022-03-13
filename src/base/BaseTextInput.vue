377e0a47c09ca86a30238f82028ec05da99eb540
Peter Khalil <uni.03c9@gmail.com>
2022-03-13 09:31:33 +0200

    disable the default inharit attributes

--- src/base/BaseTextInput.vue
+++ src/base/BaseTextInput.vue
@@ -1,22 +1,30 @@
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
+
 <script setup lang="ts">
 defineProps(["isFocused", "inSelectedCard"]);
 </script>
 
 <template>
-  <input
-    v-bind:="$attrs"
-    class="w-full outline-none"
-    @focus="isFocused = true"
-    @blur="isFocused = false"
-  />
-  <div class="w-full relative">
-    <div
-      class="w-full h-[1px] absolute top-0 left-0 z-0"
-      :class="inSelectedCard ? 'bg-gray-300' : 'bg-transparent'"
+  <div>
+    <input
+      v-bind:="$attrs"
+      class="w-full outline-none"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
     />
-    <div
-      class="top-0 h-[2px] transition-[width] relative ease-in duration-300 mx-auto z-10"
-      :class="isFocused ? 'w-full bg-primary-dark' : 'w-0 bg-transparent'"
-    />
+    <div class="w-full relative">
+      <div
+        class="w-full h-[1px] absolute top-0 left-0 z-0"
+        :class="inSelectedCard ? 'bg-gray-300' : 'bg-transparent'"
+      />
+      <div
+        class="top-0 h-[2px] transition-[width] relative ease-in duration-300 mx-auto z-10"
+        :class="isFocused ? 'w-full bg-primary-dark' : 'w-0 bg-transparent'"
+      />
+    </div>
   </div>
 </template>
