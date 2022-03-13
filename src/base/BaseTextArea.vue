68262443c128ce815e924a29e7a8fc26d66fd7eb
Peter Khalil <uni.03c9@gmail.com>
2022-03-13 14:41:28 +0200

    adding reactivity, and replacing 	extarea with p element when the card is out of focus

--- src/base/BaseTextArea.vue
+++ src/base/BaseTextArea.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { computed, ref, StyleValue } from "vue";
+import { ref } from "vue";
 
 export default {
   inheritAttrs: false,
@@ -7,9 +7,11 @@ export default {
 </script>
 
 <script setup lang="ts">
-defineProps(["isFocused", "inSelectedCard"]);
+const props = defineProps(["inSelectedCard", "modelValue"]);
+const emit = defineEmits(["update:modelValue"]);
 
-// --- style while card is selected ---
+const isFocused = ref(false);
+
 const textArea = ref<InstanceType<typeof HTMLTextAreaElement> | null>(null);
 const textAreaHeight = ref("auto");
 function expandHeight() {
@@ -19,37 +21,42 @@ function shrinkHeight() {
   textAreaHeight.value = "auto";
 }
 
-// --- style while card is NOT selected ---
+function handleInput(event: Event) {
+  emit("update:modelValue", (event.target as HTMLTextAreaElement).value);
+  expandHeight();
+}
+
+function focusOnTextArea() {
+  // TODO: not working correctly, should look at it later.
+  textArea.value?.focus();
+}
 </script>
 
 <template>
-  <div class="flex-grow flex flex-col relative">
-    <textarea
-      ref="textArea"
-      v-bind:="$attrs"
-      class="w-full h-auto outline-none overflow-hidden resize-none rounded-t-lg"
-      :class="
-        inSelectedCard
-          ? 'px-4 pt-3 leading-5 bg-gray-50 hover:bg-gray-100'
-          : 'placeholder:text-gray-800 bg-transparent cursor-pointer'
-      "
-      :style="{ height: textAreaHeight }"
-      @focus="isFocused = true"
-      @blur="isFocused = false"
-      @input="expandHeight"
-      @keydown.backspace="shrinkHeight"
-      @keydown.ctrl.z="shrinkHeight"
-      @keydown.enter.prevent
-    />
-    <div class="w-full relative">
-      <div
-        class="w-full h-[1px] absolute top-0 left-0 z-0"
-        :class="inSelectedCard ? 'bg-gray-400' : 'bg-transparent'"
+  <div class="flex-grow">
+    <p v-show="!inSelectedCard" @click="focusOnTextArea">
+      {{ modelValue ? modelValue : "Question" }}
+    </p>
+    <div v-show="inSelectedCard" class="flex flex-col relative">
+      <textarea
+        ref="textArea"
+        v-bind:="$attrs"
+        class="w-full h-auto outline-none overflow-hidden resize-none rounded-t-lg pt-3 px-4 leading-5 bg-gray-50 hover:bg-gray-100"
+        :style="{ height: textAreaHeight }"
+        @focus="isFocused = true"
+        @blur="isFocused = false"
+        @input="handleInput"
+        @keydown.backspace="shrinkHeight"
+        @keydown.ctrl.z="shrinkHeight"
+        @keydown.enter.prevent
       />
-      <div
-        class="top-0 h-[2px] transition-[width] relative ease-in duration-300 mx-auto z-10"
-        :class="isFocused ? 'w-full bg-primary-dark' : 'w-0 bg-transparent'"
-      />
+      <div class="w-full relative">
+        <div class="w-full h-[1px] absolute top-0 left-0 z-0 bg-gray-400" />
+        <div
+          class="top-0 h-[2px] transition-[width] relative ease-in duration-300 mx-auto z-10"
+          :class="isFocused ? 'w-full bg-primary-dark' : 'w-0 bg-transparent'"
+        />
+      </div>
     </div>
   </div>
 </template>
