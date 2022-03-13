ad9528e1b08415c64fd32c769982f6e4d60b1931
Peter Khalil <uni.03c9@gmail.com>
2022-03-13 09:29:58 +0200

    include roboto inside tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "Helvetica", "Aria", "sans-serif"],
+    },
     extend: {
       colors: {
         "primary-dark": "#673ab7",
